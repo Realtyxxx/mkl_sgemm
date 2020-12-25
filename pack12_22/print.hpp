d59bf09130d9da443d9e240ed37158e787d32f36
realtyxxx <841344563@qq.com>
2020-12-24 19:32:48 -0800

    " "

--- pack12_22/print.hpp
+++ pack12_22/print.hpp
@@ -3,14 +3,6 @@
 
 using namespace std;
 
-void printMatrixA(const float* matrix, int l, int m) {
-  for (int i = 0; i < l; i++) {
-    for (int j = 0; j < m; j++) {
-      cout << matrix[i * m + j] << " ";
-    }
-    cout << endl;
-  }
-}
 
 void printVector(vector<float> ve, int m) {
   for (int i = 0; i < ve.size(); i++) {
